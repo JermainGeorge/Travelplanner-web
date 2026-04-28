e53127977827fbf87544a4413fffe04702e71736
Jermain George <geojermain@gmail.com>
2026-04-28 16:52:47 +0300

    Forms in Booking update

--- Travelplanner/Bookings/forms.py
+++ Travelplanner/Bookings/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from django.utils import timezone
-from .models import Booking
+from .models import Booking , Destination, Accommodation, Vehicle
 
+#create forms for each model to be used in the views and templates
+class DestinationFrom(forms.ModelForm):
+    class Meta:
+        model = Destination
+        fields = ['name', 'location']
+
+class AccommodationForm(forms.ModelForm):
+    class Meta:
+        model = Accommodation
+        fields = ['name', 'location', 'type', 'price_per_night']
+
+class VehicleForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = ['name', 'vehicle_type', 'capacity', 'price_per_day']
 
 class BookingForm(forms.ModelForm):
     date = forms.DateField(
--- Travelplanner/Bookings/views.py
+++ Travelplanner/Bookings/views.py
@@ -1,25 +1,45 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import BookingForm
-from .models import Booking
+from .forms import BookingForm, DestinationFrom, AccommodationForm, VehicleForm
+from .models import Booking, Destination, Accommodation, Vehicle
 
-
-
-
+# Creating views for each form to handle the booking process and display receipts
 @login_required
-def  bookings(request):
+def bookings(request):
+    d_form = DestinationFrom()
+    a_form = AccommodationForm()
+    v_form = VehicleForm()
+
     if request.method == 'POST':
-        form = BookingForm(request.POST)
-        if form.is_valid():
-            booking = form.save(commit=False)
-            booking.user = request.user
-            booking.save()
+        if 'destination_submit' in request.POST:
+            d_form = DestinationFrom(request.POST)
+            if d_form.is_valid():
+                obj = d_form.save(commit=False)
+                obj.user = request.user
+                obj.save()
+                return redirect('receipts')
 
-            return redirect('receipts')  # go to receipts page
-    else:
-        form = BookingForm()
+        elif 'accommodation_submit' in request.POST:
+            a_form = AccommodationForm(request.POST)
+            if a_form.is_valid():
+                obj = a_form.save(commit=False)
+                obj.user = request.user
+                obj.save()
+                return redirect('receipts')
 
-    return render(request, 'Bookings/bookings.html', {'form': form})
+        elif 'vehicle_submit' in request.POST:
+            v_form = VehicleForm(request.POST)
+            if v_form.is_valid():
+                obj = v_form.save(commit=False)
+                obj.user = request.user
+                obj.save()
+                return redirect('receipts')
+
+    return render(request, 'Bookings/bookings.html', {
+        'd_form': d_form,
+        'a_form': a_form,
+        'v_form': v_form,
+    })
 
 
 @login_required
