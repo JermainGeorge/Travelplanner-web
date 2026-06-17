f5c89e429a5c84b22ad4a06a7b0756485882a2c6
Jermain George <geojermain@gmail.com>
2026-06-17 21:10:10 +0300

    url-maping-on-home-page

--- Travelplanner/planner/urls.py
+++ Travelplanner/planner/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns =[
     path('', views.planner, name='planner-home'),
     path('about/', views.about, name='planner-about'),
+    path('popular-destinations/', views.popular_destinations, name='popular-destinations'),
+    path('newsletter/', views.newsletter, name='newsletter'),
   
     
     
--- Travelplanner/planner/views.py
+++ Travelplanner/planner/views.py
@@ -4,6 +4,10 @@ def planner(request):
     return render(request, 'planner/home.html')
 def about(request):
     return render(request, 'planner/about.html')
+def popular_destinations(request):
+    return render(request, 'planner/p_destinations.html')
+def newsletter(request):
+    return render(request, 'planner/newsletter.html')
 
 
 
